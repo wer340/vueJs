2f7753281572798dd3153151716625c7e3db82ca
hossein.s <hsee1372@gmail.com>
2020-03-02 12:29:06 +0330

    monstarattaackhardcodetocleancode

--- monstar_project/vue.js
+++ monstar_project/vue.js
@@ -15,35 +15,63 @@ gameStart:function(){
    
 },
 attack:function(){
-    var max=10;
-   var min=3;
-  
-    var demage=Math.max(Math.floor( Math.random()* max)+1,min);
-    this.monstarBar -=demage;
-    if( this.monstarBar<=0){
-        alert('you won!');
-        this.gameRuning=false;
-        return
-    }
-    max=13;
-     min=5;
-    
-    demage=Math.max(Math.floor( Math.random()* max)+1,min);
-    this.healthBar -=demage;
-    if( this.healthBar<=0){
-        alert('you lost!');
-        this.gameRuning=false;
-       
-    }
+
+    this.monstarBar -=this.calcDemage(3,10);
+
+
+
+    this.healthBar -=this.calcDemage(5,13);
+    this.checkWin();
+   
 },
 specialAttack:function(){
+    this.monstarBar -=this.calcDemage(6,17);
 
+
+
+    this.healthBar -=this.calcDemage(5,13);
+    this.checkWin();
 },
 heal:function(){
-
+if(this.healthBar<=90){
+    this.healthBar +=10;
+   
+}else{
+    this.healthBar =100;
+}
+this.healthBar -=this.calcDemage(5,13);
+this.checkWin();
 },
 giveUp:function(){
 
+},
+calcDemage:function(min,max){
+return Math.max(Math.floor( Math.random()* max)+1,min);
+}
+
+
+
+,
+checkWin:function(){
+    if(this.monstarBar<=0){
+        if(confirm("You win! start again?")){
+            this.gameStart();
+        }
+        else{
+            this.gameRuning=false;
+        }
+        return true;
+    }
+        else if(this.healthBar<=0){
+            if(confirm("You lost! start again?")){
+                this.gameStart();
+            }
+            else{
+                this.gameRuning=false;
+            }
+            return true;   
+        }
+    return false;
 }
 
 }
