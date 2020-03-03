4a1a9c82c432097d751467c999c7cb107084541c
hossein.s <hsee1372@gmail.com>
2020-03-03 11:24:12 +0330

    finishGameCleanCode

--- monstar_project/vue.js
+++ monstar_project/vue.js
@@ -4,6 +4,7 @@ data:{
     healthBar:100,
     monstarBar:100,
     gameRuning:false,
+    turns:[],
 }
 ,
 methods:{
@@ -12,38 +13,43 @@ gameStart:function(){
     this.gameRuning=true;
     this.healthBar=100;
     this.monstarBar=100;
+    this.turns=[];
    
 },
 attack:function(){
+var demage=this.calcDemage(3,10);
+    this.monstarBar -=demage
+        this.turns.unshift({isplayer:true,text:'player hints monster demage' +demage});
 
-    this.monstarBar -=this.calcDemage(3,10);
-
-
-
-    this.healthBar -=this.calcDemage(5,13);
+    demage=this.calcDemage(5,13);
+    this.healthBar -=demage
+    this.turns.unshift({isplayer:false,text:'player is demage' +demage});
     this.checkWin();
    
 },
 specialAttack:function(){
-    this.monstarBar -=this.calcDemage(6,17);
+    var demage=this.calcDemage(6,17);
+    this.monstarBar -=demage;
+    this.turns.unshift({isplayer:true,text:'player hints monster demage special attack' +demage});
 
-
-
-    this.healthBar -=this.calcDemage(5,13);
+    demage=this.calcDemage(5,13);
+    this.healthBar -=demage;
+    this.turns.unshift({isplayer:false,text:'player is  demage in special attack' +demage});
     this.checkWin();
 },
 heal:function(){
 if(this.healthBar<=90){
     this.healthBar +=10;
-   
+    this.turns.unshift({isplayer:true,text:'player is  heal  ' +10});
 }else{
     this.healthBar =100;
 }
-this.healthBar -=this.calcDemage(5,13);
+this.healthBar -=demage;
+this.turns.unshift({isplayer:false,text:'player is  demage  ' + demage});
 this.checkWin();
 },
 giveUp:function(){
-
+this.gameRuning=false;
 },
 calcDemage:function(min,max){
 return Math.max(Math.floor( Math.random()* max)+1,min);
